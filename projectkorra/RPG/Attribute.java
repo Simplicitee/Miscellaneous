0739aaaad43a15f482095227d4c1ace2a71a9378
Simplicitee <simplicitee123@gmail.com>
2017-01-20 13:07:03 -0500

    Update Attribute.java

--- projectkorra/RPG/Attribute.java
+++ projectkorra/RPG/Attribute.java
@@ -3,4 +3,32 @@ package com.projectkorra.projectkorra.util;
 /*imports*/
 
 public class Attribute {
+  
+  private static Map<String, TYPE> attributes = new HashMap<>();
+  
+  public List<AttributeModifier> modifiers;
+  public CoreAbility ability;
+  public String name;
+  public TYPE value;
+  
+  public Attribute(CoreAbility ability, String name, TYPE value, AttributeModifier... modifiers) {
+    this.ability = ability;
+    this.name = name;
+    this.value = value;
+    attributes.put(ability.getName() + "." + name, value);
+    this.modifiers = new ArrayList<>(modifiers);
+  }
+  
+  /*Getter methods*/
+  
+  public TYPE modify(BendingPlayer bPlayer) {
+    TYPE modified = value;
+    for (AttributeModifier modifier : modifiers) {
+      if (!modifier.canModifiy(bPlayer)) {
+        continue;
+      }
+      value = modifier.newValue(value);
+    }
+    
+    return modified;
 }
