f50908af031c815d0610996596644262e80e594f
Simplicitee <simplicitee123@gmail.com>
2017-03-15 01:25:37 -0400

    Update AttributeModifier.java

--- projectkorra/RPG/AttributeModifier.java
+++ projectkorra/RPG/AttributeModifier.java
@@ -1,3 +1,12 @@
-public class AttributeModifier<TYPE> {
+public abstract class AttributeModifier {
   
+  private Attribute a;
+  private Object value;
+  
+  public AttributeModifier(Attribute a) {
+    this.a = a;
+
+  }
+  
+  public void modify
 }
