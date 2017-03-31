e7ecece2681341af88af0e3ce2bfc20f2bfb1700
Simplicitee <simplicitee123@gmail.com>
2017-03-31 12:29:38 -0400

    Update MultiAbility.java

--- projectkorra/MultiAbility.java
+++ projectkorra/MultiAbility.java
@@ -1,1 +1,15 @@
-
+public interface MultiAbility {
+  
+  public interface MultiSubAbility {
+    public MultiAbility getMultiAbility();
+  }
+  
+  public enum MultiAbilityEnder {
+    SHIFT_CLICK, DOUBLE_CLICK, DOUBLE_SHIFT, CUSTOM;
+  }
+  
+  public List<CoreAbility> getSubAbilities();
+  public MultiAbilityEnder getEnder();
+  public int getStartingSlot();
+}
+  
