9df259e206f109de4087b21dd748c7f636a95506
Simplicitee <simplicitee123@gmail.com>
2017-03-31 13:11:08 -0400

    Update MultiAbility.java

--- projectkorra/MultiAbility.java
+++ projectkorra/MultiAbility.java
@@ -1,4 +1,6 @@
-public interface MultiAbility {
+public abstract class MultiAbility extends CoreAbility{
+  
+  public List<CoreAbility> prevAbilities;
   
   public interface MultiSubAbility {
     public MultiAbility getMultiAbility();
@@ -8,8 +10,43 @@ public interface MultiAbility {
     SHIFT_CLICK, DOUBLE_CLICK, DOUBLE_SHIFT, CUSTOM;
   }
   
-  public List<CoreAbility> getSubAbilities();
-  public MultiAbilityEnder getEnder();
-  public int getStartingSlot();
+  public abstract List<CoreAbility> getSubAbilities();
+  public abstract MultiAbilityEnder getEnder();
+  public abstract int getStartingSlot();
+  
+  public MultiAbility(Player player) {
+    super(player);
+    prevAbilities = new ArrayList<>();
+    prevAbilities.addAll(bPlayer.getAbilities().values());
+    bind();
+  }
+  
+  @Override
+  public void remove() {
+    super.remove();
+    unbind();
+  }
+  
+  public void bind() {
+    bPlayer.getAbilities().clear();
+    int i = 1;
+    for (CoreAbility ability : getSubAbilities()) {
+      bPlayer.getAbilities().put(i, ability.getName());
+    }
+    player.getInventory().setHeldItemSlot(getStartingSlot() - 1);
+  }
+  
+  public void unbind() {
+    bPlayer.getAbilities().clear();
+    int i = 1;
+    int slot = 1;
+    for (CoreAbility ability : prevAbilities) {
+      bPlayer.getAbilities().put(i, ability.getName());
+      if (ability instanceof this) {
+        slot = i;
+      }
+    }
+    player.getInventory().setHeldItemSlot(slot - 1);
+  }
 }
   
