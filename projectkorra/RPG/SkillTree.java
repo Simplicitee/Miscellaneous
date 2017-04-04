63a997f020b62b60a8615929bb641e50f853b4ad
Simplicitee <simplicitee123@gmail.com>
2017-04-04 12:19:07 -0400

    Update SkillTree.java

--- projectkorra/RPG/SkillTree.java
+++ projectkorra/RPG/SkillTree.java
@@ -1,1 +1,11 @@
-
+//imports
+public interface SkillTree {
+  
+  public Player getOpener();
+  public int getSize();
+  public String getName();
+  public int getID();
+  public SkillTreeObject[] getObjects();
+  public void clickObject(SkillTreeObject object);
+  
+}
