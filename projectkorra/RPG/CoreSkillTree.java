1ca36a9cff42a5953959feef8c4d37012c156ef0
Simplicitee <simplicitee123@gmail.com>
2017-04-04 12:23:56 -0400

    Update CoreSkillTree.java

--- projectkorra/RPG/CoreSkillTree.java
+++ projectkorra/RPG/CoreSkillTree.java
@@ -1,1 +1,23 @@
-
+//imports
+public abstract class CoreSkillTree implements SkillTree {
+  
+  private static Map<String, CoreSkillTree> nameMap = new HashMap<>();
+  private static Map<Integer, CoreSkillTree> idMap = new HashMap<>();
+  private static Map<Class<? extends CoreSkillTree>, CoreSkillTree> classMap = new HashMap<>();
+  
+  private String name;
+  private int id, size;
+  
+  public CoreSkillTree(String name, int id, int size) {
+    this.name = name;
+    this.id = id;
+    this.size = size;
+    load();
+  }
+  
+  private void load() {
+    nameMap.put(name, this);
+    idMap.put(id, this);
+    classMap.put(this.getClass(), this);
+  }
+}
