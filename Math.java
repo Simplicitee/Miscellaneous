f29f1c2650e71f895fc6f0ca1b7353defbf132da
Simplicitee <simplicitee123@gmail.com>
2017-10-30 09:09:26 -0400

    Update Math.java

--- Math.java
+++ Math.java
@@ -6,16 +6,24 @@ public class Math {
         this.x = x;
     }
     
-    public int add(int y) {
-        return x+y;
+    public int add(int... y) {
+        int result = x;
+        for (int i : y) {
+            result += i;
+        }
+        return result;
     }
     
     public int subtract(int y, boolean yfromx) {
         return yfromx? x-y : y-x;
     }
     
-    public int multiply(int y) {
-        return x*y;
+    public int multiply(int... y) {
+        int result = x;
+        for (int i : y) {
+            result *= i;
+        }
+        return result;
     }
     
     public double divide(int y, boolean yfromx) {
@@ -24,11 +32,37 @@ public class Math {
     
     public int modulo(int y, boolean yfromx) {
         return yfromx? x%y : y%x;
+    }
     
     public int factorial() {
         int result = 1;
-        for (int n = x; n ≥ 1; n--) {
+        for (int n = x; n >= 1; n--) {
             result *= n;
+        }
+        return result;
+    }
+    
+    public int power(int n) {
+        int result = 1, a = n >= 0 ? n : (n*-1);
+        while (a > 0) {
+            result *= x;
+            a--;
+        }
+        return n >= 0 ? result : 1/result;
+    }
+    
+    /**
+    *   @param base The base you want to check for if factors
+    *               for your number exist. (ex: x=9, base=3)
+    */
+    public int root(int n, int base) {
+        int result = x;
+        while (n > 0) {
+            if (result%base == 0) {
+                result /= base;
+            } else {
+                break;
+            }
         }
         return result;
     }
